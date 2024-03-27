252ad8e0af8d9f1f2b25ce9a108eeda6e91a7b86
Michael Tarimo, MD, Dev <56628755+Michaeltarimo@users.noreply.github.com>
2024-03-27 15:58:57 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
+const path = require('path'); // Import the path module
 
-const port = 3009;
+const port = process.env.PORT || 3009; // Use the port provided by the environment or default to 3009
 
 const app = express();
 app.use(cors({ origin: 'http://localhost:3009' }));
 
-app.use('/', express.static(__dirname));
-
+// Define routes
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html')
+    res.sendFile(path.join(__dirname, 'index.html')); // Use path.join to construct the file path
 });
-app.get('/agent', function(req, res) {
-    res.sendFile(__dirname + '/index-agents.html')
+
+app.get('/agents', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index-agents.html')); // Use path.join to construct the file path
 });
 
 const server = http.createServer(app);
